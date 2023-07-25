e7fbefa02bca2133c3b3a508ccb48e8b8cc8acc3
Armin <ketanovic@icloud.com>
2023-07-25 17:32:48 -0500

    Sales_Rest views

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import Salesperson
 
-# Create your views here.
+
+class SalespersonEncoder(ModelEncoder):
+    model = Salesperson
+    properties = [
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+
+@require_http_methods(["GET", "POST"])
+def api_salesperson(request):
+    if request.method == "GET":
+        salesperson = Salesperson.objects.all()
+        return JsonResponse(
+            {"saleperson": salesperson},
+            encoder=SalespersonEncoder
+        )
+    else:
+        content = json.loads(request.body)
+        employee = Salesperson.objects.create(**content)
+        return JsonResponse(
+            employee,
+            encoder=SalespersonEncoder,
+            safe=False,
+        )
