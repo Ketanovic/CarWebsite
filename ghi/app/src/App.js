b6e366b022e4d359866a86296d4f9af822485782
Armin <ketanovic@icloud.com>
2023-07-28 10:11:54 -0500

    Restored my views.

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,41 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
-import SalespeopleForm from './CreateSalespersonForm';
-import SalespeopleList from './SalespeopleList';
-import ManufacturersList from './ManufacturersList';
-import ManufacturerForm from './ManufacturerForm';
-import TechniciansList from './TechniciansList';
-import TechnicianForm from './TechnicianForm';
-import ModelForm from './CreateModelForm';
-import ModelsList from './ModelsList';
-import AutomobilesList from './AutomobilesList';
-import AutomobileForm from './CreateAutomobileForm';
-import AppointmentsList from './AppointmentsList';
-import AppointmentForm from './AppointmentForm';
 import Nav from './Nav';
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [models, setModels] = useState([]);
-  const [automobiles, setAutomobiles] = useState([]);
-
-  async function getModels() {
-    const modelUrl = 'http://localhost:8100/api/models/'
-    const response = await fetch(modelUrl)
-    if (response.ok) {
-      const data = await response.json();
-      setModels(data.models)
-    }
-  }
-    async function getAutomobiles() {
-    const modelUrl = 'http://localhost:8100/api/automobiles/'
-    const response = await fetch(modelUrl)
-    if (response.ok) {
-      const data = await response.json();
-      setAutomobiles(data.automobiles)
-    }
-  }
-
 
   useEffect(() => {
     getModels();
@@ -48,20 +16,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/salespeople" element={<SalespeopleList salespeople={salespeople} />} />
-          <Route path="/salespeople/create" element={<SalespersonForm getSalespeople={getSalespeople} />} />
-          <Route path="/manufacturers" element={<ManufacturersList />} />
-          <Route path="/manufacturers/create" element={<ManufacturerForm />} />
-          <Route path="/technicians" element={<TechniciansList />} />
-          <Route path="/technicians/create" element={<TechnicianForm />} />
-          <Route path="/models" element={<ModelsList />} />
-          <Route path="/models/create" element={<ModelForm getModels={getModels} />} />
-          <Route path="/automobiles" element={<AutomobilesList getAutomobiles={getAutomobiles} />} />
-          <Route path="/automobiles/create" element={<AutomobileForm getAutomobiles={getAutomobiles} />} />
-          <Route path="/appointments" element={<AppointmentsList />} />
-          <Route path="/appointments/create" element={<AppointmentForm />} />
         </Routes>
       </div>
     </BrowserRouter>
   );
 }
+
+export default App;
