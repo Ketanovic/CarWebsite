86d1b96c402c477741d3376c0168d681e8f057f4
Armin <ketanovic@icloud.com>
2023-07-28 13:25:31 -0500

    Finished JS for ListCustomers and CreateCustomerForm

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -4,9 +4,12 @@ import MainPage from './MainPage';
 import Nav from './Nav';
 import SalespersonForm from './CreateSalespersonForm';
 import Salespeople from './ListSalespeople';
+import CustomerForm from './CreateCustomerForm';
+import Customers from './ListCustomers';
 
 function App() {
   const [salespeople, setSalespeople] = useState([]);
+  const [customers, setCustomers] = useState([]);
 
   async function fetchSalespeople() {
     const salespeopleUrl = 'http://localhost:8090/api/salespeople/';
@@ -17,8 +20,18 @@ function App() {
     }
   }
 
+  async function fetchCustomers() {
+    const customersUrl = 'http://localhost:8090/api/customers/';
+    const response = await fetch(customersUrl);
+    if (response.ok) {
+      const data = await response.json();
+      setCustomers(data.customers);
+    }
+  }
+
   useEffect(() => {
     fetchSalespeople();
+    fetchCustomers();
   }, []);
 
   return (
@@ -27,23 +40,10 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-<<<<<<< HEAD
           <Route path="/add-salesperson" element={<SalespersonForm />} />
           <Route path="/salespeople" element={<Salespeople />} />
-=======
-          <Route path="/salespeople" element={<SalespeopleList salespeople={salespeople} />} />
-          <Route path="/salespeople/create" element={<SalespersonForm getSalespeople={getSalespeople} />} />
-          <Route path="/manufacturers" element={<ManufacturersList />} />
-          <Route path="/manufacturers/create" element={<ManufacturerForm />} />
-          <Route path="/technicians" element={<ListTechnician />} />
-          <Route path="/technicians/create" element={<AddTechnician />} />
-          <Route path="/models" element={<ModelsList />} />
-          <Route path="/models/create" element={<ModelForm getModels={getModels} />} />
-          <Route path="/automobiles" element={<AutomobilesList getAutomobiles={getAutomobiles} />} />
-          <Route path="/automobiles/create" element={<AutomobileForm getAutomobiles={getAutomobiles} />} />
-          <Route path="/appointments" element={<AppointmentsList />} />
-          <Route path="/appointments/create" element={<AppointmentForm />} />
->>>>>>> a83c001945c862beffb591cd8ab4de82ab7187bf
+          <Route path="/add-customer" element={<CustomerForm />} />
+          <Route path="/customers" element={<Customers />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/ListCustomers.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+function ListCustomers() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    async function fetchCustomers() {
+        try {
+            const response = await fetch('http://localhost:8090/customers/');
+            if (!response.ok) {
+            throw new Error('Failed to fetch customers data');
+        }
+        const data = await response.json();
+        setCustomers(data.Customer);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  fetchCustomers();
+  }, []);
+  return (
+    <div>
+      <h1>List of Customers</h1>
+      {customers.length === 0 ? (
+        <p>No customers found.</p>
+      ) : (
+        <ul className="customers-list">
+          {customers.map((customer) => (
+            <li key={customer.id}>
+              <strong>{customer.first_name} {customer.last_name}</strong><br />
+              <span>Email: {customer.email}</span><br />
+              <span>Phone: {customer.phone}</span><br />
+              <span>Customer ID: {customer.id}</span><br />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default ListCustomers;
